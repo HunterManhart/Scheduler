7d3edd83f7d58970fce02727177618a4a4d20a0d
Hunter Manhart <Hunter.M.Manhart@vanderbilt.edu>
2017-06-15 09:34:07 -0500

    needs some interface work

---
+++ scheduler.py
@@ -0,0 +1,91 @@
+from pulp import *
+import xlrd
+
+# pulp.pulpTestAll()
+
+#       Initial Data
+# Open the workbook and define the worksheet
+book = xlrd.open_workbook("input/practice.xlsx")
+sheet = book.sheet_by_name("Sheet1")
+
+meetings = [ [sheet.cell(student, firm).value for student in range(1, sheet.nrows)] for firm in range(1, sheet.ncols)]
+
+# meetings = [
+#     [1, 0, 1, 0],
+#     [1, 1, 0, 1],
+#     [1, 1, 1, 1]
+# ]
+
+# print(meetings)
+
+firms = range(0, len(meetings))
+students = range(0, len(meetings[0]))
+
+
+#       Processed Data
+# Timeslots in a day
+timeslots = 20
+# List to record variables in use
+possible_slots = []
+
+# Loop through meetings
+for i, firmList in enumerate(meetings):
+    firm = firms[i]
+    for j, meeting in enumerate(firmList):
+        student = students[j]
+        # If meeting is needed between firm and student, add fst slots to variables
+        if meeting == 1:
+            possible_slots += [(firm, student, slotNum) for slotNum in range(1, timeslots)]
+
+# Binary variable to state that a firm-student-timeslot is set
+fst = pulp.LpVariable.dicts('fst', possible_slots, 
+                            lowBound = 0,
+                            upBound = 1,
+                            cat = pulp.LpInteger)
+
+
+scheduler = LpProblem("Scheduler Problem", LpMinimize)
+
+#       Optimization (later slots have more weight)
+scheduler += lpSum([fst[slot] * slot[2] for slot in possible_slots]), "Weight on time firm needs to spend"
+
+#       Constraints
+
+# Students don't overlap meetings and have breaks
+for student in students:
+    for slotNum in range(1, timeslots-1):
+        scheduler += lpSum([fst[(firm, student, slotNum)] for firm in firms if meetings[firm][student] == 1] + [fst[(firm, student, slotNum + 1)] for firm in firms if meetings[firm][student] == 1]) <= 1
+
+# Firms don't have more than 1 student in a timeslot
+for firm in firms:
+    for slotNum in range(1, timeslots):
+        scheduler += lpSum([fst[(firm, student, slotNum)] for student in students if meetings[firm][student] == 1]) <= 1
+
+# Each interview must be in only one timeslot
+for i, firmList in enumerate(meetings):
+    firm = firms[i]
+    for j, meeting in enumerate(firmList):
+        student = students[j]
+        # For each meeting, make sure fst (ie all potential slots) sums to 1
+        if meeting == 1:
+            scheduler += lpSum([fst[(firm, student, slotNum)] for slotNum in range(1, timeslots)]) == 1
+
+
+
+scheduler.solve()
+
+ftSlots = {}
+for slot in possible_slots:
+    if fst[slot].value() == 1:
+        print(slot)
+        ftSlots[(slot[0], slot[2])] = slot[1]
+
+for slot in range(1, timeslots):
+    string = ""
+    for firm in firms:
+        if (firm, slot) in ftSlots:
+            string += str(ftSlots[(firm, slot)] + 1)
+        else:
+            string += " "
+        string += "  "
+    print string
